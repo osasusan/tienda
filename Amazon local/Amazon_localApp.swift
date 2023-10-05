4de911ba8d1657d8b79e8abf583954aeb7440663
osasusan <107266592+osasusan@users.noreply.github.com>
2023-10-05 23:59:31 +0200

    Initial Commit

---
+++ Amazon local/Amazon_localApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amazon_localApp.swift
+//  Amazon local
+//
+//  Created by Osasu sanchez on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Amazon_localApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
